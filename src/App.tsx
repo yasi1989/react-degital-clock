8a3b21c3262241bf3df2b9acdc0f73dba9d334aa
yasi1989 <kei.kobayashi.works1989@gmail.com>
2024-12-18 23:16:49 +0900

    ディレクトリ構成変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import DegitalClock from "./pages/DegitalClock";
+import DegitalClock from "./app/DegitalClock";
 
 function App() {
   return (
